8488a9acf1468fd3bfb19c681c91f04a8a3da25c
Stiouf <stephane.quentin@live.fr>
2018-10-10 14:38:42 +0200

    correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,21 @@
-function survol(){
-  document.getElementById('image1').src = 'assets/img/image1_2.jpg';
-  document.getElementById('image2').src = 'assets/img/image2_2.jpg';
-  document.getElementById('image3').src = 'assets/img/image3_2.jpg';
-  document.getElementById('image4').src = 'assets/img/image4_2.jpg';
-  document.getElementById('image5').src = 'assets/img/image5_2.jpg';
+// function survol(){
+//   document.getElementById('image1').src = 'assets/img/image1_2.jpg';
+//   document.getElementById('image2').src = 'assets/img/image2_2.jpg';
+//   document.getElementById('image3').src = 'assets/img/image3_2.jpg';
+//   document.getElementById('image4').src = 'assets/img/image4_2.jpg';
+//   document.getElementById('image5').src = 'assets/img/image5_2.jpg';
+// }
+// function nosurvol(){
+//   document.getElementById('image1').src = 'assets/img/image1.jpg';
+//   document.getElementById('image2').src = 'assets/img/image2.jpg';
+//   document.getElementById('image3').src = 'assets/img/image3.jpg';
+//   document.getElementById('image4').src = 'assets/img/image4.jpg';
+//   document.getElementById('image5').src = 'assets/img/image5.jpg';
+// }
+
+function survol(img){
+  img.src = 'assets/img/' + img.id + '_2.jpg';
 }
-function nosurvol(){
-  document.getElementById('image1').src = 'assets/img/image1.jpg';
-  document.getElementById('image2').src = 'assets/img/image2.jpg';
-  document.getElementById('image3').src = 'assets/img/image3.jpg';
-  document.getElementById('image4').src = 'assets/img/image4.jpg';
-  document.getElementById('image5').src = 'assets/img/image5.jpg';
+function nosurvol(img){
+  img.src = 'assets/img/' + img.id + '.jpg';
 }
